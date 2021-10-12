bfeaee00ad39cbd8b3b58e6d5da1b5b1d2550dce
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-10-12 17:18:23 +0300

    refactor(Api): Api interface changed
    - Изменен интерфейс Api

--- app/src/main/java/ru/zapashnii/testjetpackcompose/network/Api.kt
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/network/Api.kt
@@ -3,9 +3,11 @@ package ru.zapashnii.testjetpackcompose.network
 import retrofit2.http.GET
 import retrofit2.http.Header
 import retrofit2.http.Query
+import ru.zapashnii.testjetpackcompose.BuildConfig
 import ru.zapashnii.testjetpackcompose.domain.model.Recipe
 import ru.zapashnii.testjetpackcompose.domain.model.RecipeResponse
 
+/** Интерфейс с договорами(контрактами) получения данных с сервера */
 interface Api {
 
     /**
@@ -13,12 +15,12 @@ interface Api {
      *
      * @param token     токен
      * @param page      номер страницы
-     * @param query     запрос из ключевых слов для поиска рецепта. Пример: beef carrot potato onion
+     * @param query     запрос из ключевых слов для поиска рецепта. Пример: "beef carrot potato onion"
      * @return
      */
     @GET("search")
     suspend fun search(
-        @Header("Authorization") token: String,
+        @Header("Authorization") token: String = BuildConfig.API_KEY,
         @Query("page") page: Int,
         @Query("query") query: String,
     ): RecipeResponse
@@ -32,7 +34,7 @@ interface Api {
      */
     @GET("get")
     suspend fun get(
-        @Header("Authorization") token: String,
+        @Header("Authorization") token: String = BuildConfig.API_KEY,
         @Query("id") id: Int,
     ): Recipe
 }
