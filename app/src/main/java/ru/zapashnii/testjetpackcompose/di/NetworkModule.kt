7f331e9cfc33ad7c39901f90e8539429eef2e63e
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-10-12 14:10:57 +0300

    feat(NetworkModule): Added creation of network objects
    - Добавлено создание сетевых объектов

--- app/src/main/java/ru/zapashnii/testjetpackcompose/di/NetworkModule.kt
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/di/NetworkModule.kt
@@ -1,9 +1,47 @@
 package ru.zapashnii.testjetpackcompose.di
 
+import com.google.gson.GsonBuilder
 import dagger.Module
+import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import ru.zapashnii.testjetpackcompose.BuildConfig
+import ru.zapashnii.testjetpackcompose.network.Api
+import javax.inject.Singleton
 
 /** Класс создания сетевых объектов */
 @Module
 class NetworkModule {
 
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor =
+        HttpLoggingInterceptor().apply {
+            this.level = HttpLoggingInterceptor.Level.BODY
+        }
+
+    @Provides
+    @Singleton
+    fun provideOkhttpClient(
+        httpLoggingInterceptor: HttpLoggingInterceptor,
+    ): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRecipeRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BuildConfig.API_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideWeatherApi(retrofit: Retrofit): Api =
+        retrofit.create(Api::class.java)
 }
