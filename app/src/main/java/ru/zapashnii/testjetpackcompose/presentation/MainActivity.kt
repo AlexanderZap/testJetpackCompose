a1d32dbb74cd46088a5a8924b6560ccc416e67a2
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-10-15 15:00:31 +0300

    feat(FoodCategory): add enum class FoodCategory
    - Создан класс перечисления FoodCategory

--- app/src/main/java/ru/zapashnii/testjetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/presentation/MainActivity.kt
@@ -5,26 +5,34 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import ru.zapashnii.testjetpackcompose.ui.fields.BottomBar
+import ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list.RecipeListScreen
+import ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list.RecipeListViewModel
 import ru.zapashnii.testjetpackcompose.ui.theme.TestJetpackComposeTheme
+import ru.zapashnii.testjetpackcompose.utils.appComponent
+import javax.inject.Inject
 
 class MainActivity : ComponentActivity() {
 
     private var nameTitle: String = "Home"
 
+    @Inject
+    lateinit var viewModel: RecipeListViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        appComponent.inject(this)
+
         setContent {
             TestJetpackComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    val state = remember { mutableStateOf(nameTitle) }
+                    RecipeListScreen(viewModel)
+                   /* val state = remember { mutableStateOf(nameTitle) }
                     BottomBar(
                         onTabBarClick = { state.value = it },
                         nameTitle = state.value
-                    )
+                    )*/
                 }
             }
         }
---
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/presentation/ui/recipe_list/FoodCategory.kt
@@ -0,0 +1,55 @@
+package ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list
+
+import ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list.FoodCategory.*
+
+/**
+ * Класс перечисления категорий продуктов питания, который используется в строке поиска
+ *
+ * @property value
+ */
+enum class FoodCategory(val value: String) {
+    CHICKEN("Chicken"),
+    BEEF("Beef"),
+    SOUP("Soup"),
+    DESSERT("Dessert"),
+    VEGETARIAN("Vegetarian"),
+    MILK("Milk"),
+    VEGAN("Vegan"),
+    PIZZA("Pizza"),
+    DONUT("Donut"),
+}
+
+/**
+ * Получить список всех категорий
+ *
+ * @return  список всех категорий
+ */
+fun getAllFoodCategories(): List<FoodCategory> {
+    return listOf(CHICKEN, BEEF, SOUP, DESSERT, VEGETARIAN, MILK, VEGAN, PIZZA, DONUT)
+}
+
+/**
+ * Получить категорию по заданному значению
+ *
+ * @param value          заданное значение
+ * @return
+ */
+fun getFoodCategory(value: String): FoodCategory? {
+    val map = FoodCategory.values().associateBy(FoodCategory::value)
+    return map[value]
+}
+
+/**
+ * Получить Id категории по его названию
+ *
+ * @param value     название категории
+ * @return          Id категории
+ */
+fun getIdFoodCategory(value: String): Int {
+    for (category in getAllFoodCategories()) {
+        if (category.value == value) {
+            return getAllFoodCategories().indexOf(category)
+        }
+    }
+    return 0
+}
