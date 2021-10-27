992897094b962611ebeb29e1090bf631169f20db
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-10-27 11:56:23 +0300

    feat(Constants): Add Constants for BottomBar
    - Добавлены константы для нижней панели навигации

---
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/data/const/Constants.kt
@@ -0,0 +1,13 @@
+package ru.zapashnii.testjetpackcompose.data.const
+
+/** Карточка Settings для нижней панели навигации */
+const val SETTINGS: String = "Settings"
+
+/** Карточка Email для нижней панели навигации */
+const val EMAIL: String = "Email"
+
+/** Карточка Search для нижней панели навигации */
+const val SEARCH: String = "Search"
+
+/** Карточка Home для нижней панели навигации */
+const val HOME: String = "Home"
