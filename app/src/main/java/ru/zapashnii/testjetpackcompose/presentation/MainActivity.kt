a7baa48fc55d797243b6888abc6abd04bee53f0e
Alexander Zapashniy <aszapashniy@lotus.bank.srv>
2021-10-27 11:58:13 +0300

    feat(Scaffold): Add Scaffold
    - Добавлен Scaffold

--- app/src/main/java/ru/zapashnii/testjetpackcompose/presentation/MainActivity.kt
+++ app/src/main/java/ru/zapashnii/testjetpackcompose/presentation/MainActivity.kt
@@ -4,10 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.unit.sp
+import kotlinx.coroutines.launch
 import ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list.RecipeListScreen
 import ru.zapashnii.testjetpackcompose.presentation.ui.recipe_list.RecipeListViewModel
+import ru.zapashnii.testjetpackcompose.ui.fields.BottomBar
+import ru.zapashnii.testjetpackcompose.ui.fields.Toolbar
 import ru.zapashnii.testjetpackcompose.ui.theme.TestJetpackComposeTheme
 import ru.zapashnii.testjetpackcompose.utils.appComponent
 import javax.inject.Inject
@@ -27,14 +35,30 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             TestJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    RecipeListScreen(viewModelRecipeList)
-                    /* val state = remember { mutableStateOf(nameTitle) }
-                     BottomBar(
-                         onTabBarClick = { state.value = it },
-                         nameTitle = state.value
-                     )*/
+                    val scaffoldState = rememberScaffoldState()
+                    val scope = rememberCoroutineScope()
+
+                    Scaffold(
+                        scaffoldState = scaffoldState,
+                        topBar = {
+                            Toolbar(
+                                iconImageVector = Icons.Filled.Menu,
+                                onBackClick = { scope.launch { scaffoldState.drawerState.open() } }
+                            )
+                        },
+                        bottomBar = {
+                            val state = remember { mutableStateOf(nameTitle) }
+                            BottomBar(
+                                onTabBarClick = { state.value = it },
+                                nameTitle = state.value,
+                                countNotification = 100
+                            )
+                        },
+                        drawerContent = { Text("Пункт меню 1", fontSize = 28.sp) },
+                    ) {
+                        RecipeListScreen(viewModelRecipeList)
+                    }
                 }
             }
         }
